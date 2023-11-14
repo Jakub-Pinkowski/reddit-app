da3605144bca5dfa65024a15db766ea5060a8a72
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-11-14 23:28:08 +0100

    Update page URL in createPost function

--- e2e/utils.ts
+++ e2e/utils.ts
@@ -74,7 +74,7 @@ export async function login(
 }
 
 export async function createPost(page: Page, title: string, contents: string) {
-    page.goto('http://localhost:1337/1')
+    page.goto('http://localhost:5173/1')
     const postTitleInput = page.locator(`input[name="title"]`)
     const postContentsInput = page.locator(`textarea[name="contents"]`)
     const postSubmitButton = page.locator(`button[type="submit"]`)
