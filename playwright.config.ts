0a11190ef65160793298dcecaacb0d8180909f7a
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-11-14 20:18:01 +0100

    Add detect-port package and update e2e tests configuration

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,7 +12,7 @@ import { defineConfig, devices } from '@playwright/test'
 export default defineConfig({
     testDir: './e2e',
     /* Run tests in files in parallel */
-    fullyParallel: true,
+    fullyParallel: false,
     /* Fail the build on CI if you accidentally left test.only in the source code. */
     forbidOnly: !!process.env.CI,
     /* Retry on CI only */
@@ -71,7 +71,7 @@ export default defineConfig({
     /* Run your local dev server before starting the tests */
     webServer: {
         command: 'npm run dev',
-        url: 'http://127.0.0.1:3000',
+        url: '5173',
         reuseExistingServer: true,
     },
 })
